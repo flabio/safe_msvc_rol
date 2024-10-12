6a403543753cccc11323e9049c947b666357f563
Flabio Hinestroza Sanclemente <flabiohs@gmail.com>
2024-10-12 11:06:13 -0500

    configuracion de db

--- infrastructure/database/connectionDB.go
+++ infrastructure/database/connectionDB.go
@@ -21,7 +21,7 @@ func DatabaseConnection() *gorm.DB {
 	DB_NAME := os.Getenv("DB_NAME")
 	DB_PORT := os.Getenv("DB_PORT")
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s  sslmode=disable ",
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s  sslmode=require ",
 		DB_HOST,
 		DB_USER,
 		DB_PASSWORD,
